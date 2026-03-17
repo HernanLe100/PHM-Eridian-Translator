274e01c35a766f3f53718e12cf98a2a48624632f
Hernan Le <hernanle2006@gmail.com>
2026-03-16 23:57:02 -0400

    debugging progress

--- audio_analyzer.py
+++ audio_analyzer.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy.signal import stft
 
 SAMPLE_RATE = 44100 
-FRAME_LEN = 2048
+FRAME_LEN = 2048 # number of points being analyzed at a time for FFT
 # ----------------------------------------------------------------------
 
 # Takes audio input until user presses Enter
@@ -29,7 +29,7 @@ def record() -> list:
 
 # ----------------------------------------------------------------------
 
-# Takes array created by record() and returns two arrays:
+# Takes array produced by record() and returns two arrays:
 # time_axis is an array spanning from 0 to (SAMPLE_RATE * recording time length)
 # recording is the flattened array of recorded_data
 def time_analysis(recorded_data: list) -> tuple[list, list]:
@@ -39,7 +39,7 @@ def time_analysis(recorded_data: list) -> tuple[list, list]:
     
     return time_axis, recording
 
-# Takes array created by record() and returns two arrays:
+# Takes array produced by record() and returns two arrays:
 # frequencies is an array spanning the range of frequencies being measured
 # freq_magnitudes is an array of lists of frequencies strengths across the recording's time
 def freq_analysis(recorded_data: list) -> tuple[list, list]:
@@ -79,6 +79,10 @@ def main():
     _, f3 = freq_analysis(wave3)
     _, f4 = freq_analysis(wave4)
     _, f5 = freq_analysis(wave5)
+    
+    import audio_visualizer 
+    _, sldr = audio_visualizer.create_time_graph(time_axis, recording)
+    audio_visualizer.show_graphs()
     
 if __name__ == "__main__":
     main()
--- audio_visualizer.py
+++ audio_visualizer.py
@@ -1,18 +1,27 @@
+# audio_visualizer.py - Hernan Le
+
+# This module contains functions for playing back the audio from audio_analyzer.py 
+# and graphing the data.
+# ----------------------------------------------------------------------
+
 import sounddevice as sd
 import numpy as np
 import threading
 import math
 
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, Button
 
 import audio_analyzer as aud
 from audio_analyzer import SAMPLE_RATE, FRAME_LEN
 
 # ----------------------------------------------------------------------
 
+# thread to play audio recording produced by aud.record()
+# and display graphs if provided 
+# LIKELY TO CHANGE IN FUTURE
 class AudioPlayThread(threading.Thread):
-    def __init__(self, rec, t_sldr, f_sldr):
+    def __init__(self, rec, t_sldr= None, f_sldr=None):
         threading.Thread.__init__(self)
         self._recorded_data = rec
         self._time_slider = t_sldr
@@ -28,11 +37,18 @@ class AudioPlayThread(threading.Thread):
             if self._freq_slider:
                 self._freq_slider.set_val(self._rd_index)
         
-        with sd.OutputStream(samplerate=SAMPLE_RATE, blocksize=FRAME_LEN, channels=1, callback=callback ) as stream:
-            sd.sleep(math.ceil(1000 * len(np.array(self._recorded_data).flatten())/SAMPLE_RATE))
+        with sd.OutputStream(samplerate=SAMPLE_RATE, blocksize=FRAME_LEN, channels=1, callback=callback , latency="low") as stream:
+            # WIP 
+            while self._rd_index < len(self._recorded_data):
+                sd.sleep(math.ceil(1000 * len(np.array(self._recorded_data).flatten())/SAMPLE_RATE))
+                
+                print("go") # debugging code
+            print("end")
 
 # ----------------------------------------------------------------------
 
+# Creates a graph plotting time_axis on x and recording on y
+# Returns the figure and time slider 
 def create_time_graph(time_axis: list, recording:list):
     RECORDING_TIME = len(recording)/SAMPLE_RATE
     
@@ -47,7 +63,7 @@ def create_time_graph(time_axis: list, recording:list):
     vert_line,  = axis.plot([0,0], [-MAX_AMP,MAX_AMP])
     
     axis_slider = plt.axes([0.25, 0.1, 0.65, 0.03])
-    slider = Slider(axis_slider, "", 0, RECORDING_TIME, valstep=0.01)
+    slider = Slider(axis_slider, "Time", 0, RECORDING_TIME, valstep=0.01)
     slider.set_val(0)
     
     def update(val):
@@ -55,9 +71,12 @@ def create_time_graph(time_axis: list, recording:list):
         figure.canvas.draw_idle()
 
     slider.on_changed(update)
-    return figure, slider # need to return and store in main so garbage collector does not remove
-    
-    
+    # need to return and store in main so garbage collector does not remove
+    return figure, slider 
+
+
+# Creates a graph plotting frequencies on x and freq_magnitudes on y
+# Returns the figure and frame slider 
 def create_freq_graph(frequencies: list, freq_magnitudes:list):
     NUM_FRAMES = len(freq_magnitudes)
     LARGEST_FREQ_MAG = float( np.max( np.array(freq_magnitudes).flatten()) )
@@ -69,7 +88,7 @@ def create_freq_graph(frequencies: list, freq_magnitudes:list):
     axis.set_ylim(0, LARGEST_FREQ_MAG)
     
     axis_slider = plt.axes([0.25, 0.1, 0.65, 0.03])
-    slider = Slider(axis_slider, "", 0, NUM_FRAMES-1, valstep=1)
+    slider = Slider(axis_slider, "Frame", 0, NUM_FRAMES-1, valstep=1)
     slider.set_val(0)
     
     def update(val):
@@ -79,6 +98,16 @@ def create_freq_graph(frequencies: list, freq_magnitudes:list):
     slider.on_changed(update)
     return figure, slider
 
+# Shows all active figures of plt
+def show_graphs():
+    plt.show()
+
+# Spawns an AudioPlayThread and runs it
+# LIKELY TO CHANGE IN FUTURE
+def play(recorded_data: list, t_sldr=None, f_sldr=None):
+    audio = AudioPlayThread(recorded_data, t_sldr=t_sldr, f_sldr=f_sldr)
+    audio.start()
+        
 # ----------------------------------------------------------------------
 
 def main():
@@ -92,10 +121,9 @@ def main():
     time_plot, time_slider = create_time_graph(time_axis, recording)
     freq_plot, freq_slider = create_freq_graph(frequencies, freq_magnitudes)
     
-    audio = AudioPlayThread(recorded_data, time_slider, freq_slider)
-    audio.start()
+    play(recorded_data, t_sldr=time_slider, f_sldr=freq_slider)
     
-    plt.show()
+    show_graphs()
     
 
 if __name__ == "__main__":
