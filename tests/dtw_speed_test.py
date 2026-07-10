607ef33a5393baf4b7b303ab090af472937a2109
Hernan Le <hernanle2006@gmail.com>
2026-07-10 13:48:01 -0500

    fixed dtw-python test

--- tests/dtw_speed_test.py
+++ tests/dtw_speed_test.py
@@ -102,7 +102,7 @@ def dtw_python_speed_test(A, B, num_iter=10):
     sum = 0
     for i in range(num_iter):
         start = time.process_time()
-        dtw_python.dtw(A, B, dist_method="cosine")
+        dtw_python.dtw(A, B, dist_method="cosine", step_pattern="symmetric1")
         end = time.process_time()
         sum += end-start
     return sum / num_iter
@@ -129,7 +129,7 @@ def speed_comparison():
     print(f"Librosa score:      {librosa.sequence.dtw(s1, s2, metric="cosine")[0][-1,-1]}")
     print(f"FastDTW score:      {fastdtw.fastdtw(s1.T, s2.T, dist=cosine)[0]}")
     print(f"DTAIDistance score: {(dtai_dtw.distance(s1.T/np.linalg.norm(s1.T,axis=1,keepdims=True), s2.T/np.linalg.norm(s2.T,axis=1,keepdims=True))**2) / 2 }")
-    print(f"DTW-Python score:   {dtw_python.dtw(s1.T, s2.T, dist_method="cosine").distance}")
+    print(f"DTW-Python score:   {dtw_python.dtw(s1.T, s2.T, dist_method="cosine", step_pattern="symmetric1").distance}")
     
 # ----------------------------------------------------------------------
 
