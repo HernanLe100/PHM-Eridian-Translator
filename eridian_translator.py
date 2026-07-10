0817e94fcd09a0be4e60d03d1f5344c9ef1545b4
Hernan Le <hernanle2006@gmail.com>
2026-07-09 19:56:07 -0500

    add instructions and notes

--- eridian_translator.py
+++ eridian_translator.py
@@ -1,5 +1,25 @@
 # eridian_translator.py - Hernan Le
 
+# This script takes in audio and matches recording to words in an AudioDictionary,
+# printing the word to the terminal. If no match is found, "???" will be printed. 
+
+# When encountering a "???", assign a word to the audio by entering the 
+# word enclosed by angle brackets.
+# For example: "<word>"
+
+# The program will continue parsing words, which may visually conflict with 
+# user input, but upon submitting the word, only the user inputted text will be processed.
+
+# To skip assigning a word to audio, simply press Enter. A reminder of 
+# how many "???" there are left to be assigned a word will be printed.
+
+# Without angle brackets, the program will also skip assigning a word to audio.
+
+# Avoid pressing Enter multiple times for a single "???" because doing so will 
+# actually queue input for future "???". 
+# For instance, pressing Enter 5 times will queue up inputs (empty inputs: "") for 
+# the next 5 "???" and will automatically apply those inputs.
+
 # ----------------------------------------------------------------------
 
 from threading import Thread, Lock
@@ -10,6 +30,7 @@ from audio_dictionary import AudioDictionary
 
 # ----------------------------------------------------------------------
 
+# records words and adds to rec_queue
 class RecordingThread(Thread):
     def __init__(self, rec_queue:Queue):
         Thread.__init__(self, daemon=True)
@@ -19,21 +40,22 @@ class RecordingThread(Thread):
         while True:
             recording = record_word()
             self._rec_queue.put(recording)
-        
+
+# tries to find word in audio dictionary, print word if found, send to add_queue otherwise
 class MatchingThread(Thread):
-    def __init__(self, rec_queue:Queue, add_queue:Queue, aud:AudioDictionary, lock:Lock):
+    def __init__(self, rec_queue:Queue, add_queue:Queue, aud:AudioDictionary, aud_lock:Lock):
         Thread.__init__(self, daemon=True)
         self._rec_queue = rec_queue
         self._add_queue = add_queue
         self._aud = aud
-        self._lock = lock
+        self._aud_lock = aud_lock
     
     def run(self):
         while True:
             # Dequeue data. Makes thread sleep if no data
             rec = self._rec_queue .get()
             
-            with self._lock:
+            with self._aud_lock:
                 res = self._aud.match_recording(rec)
                 
                 if res["value"] is not None:
@@ -50,13 +72,13 @@ class MatchingThread(Thread):
 
 # stdin/stdout on main thread
 def main():
-    aud = AudioDictionary(data_name="words2", reset=False)
+    aud = AudioDictionary(data_name="words", reset=False)
+    aud_lock = Lock() # prevent race conditions with audio dictionary
     rec_queue = Queue()
     add_queue = Queue()
-    lock = Lock()
     
     recording_thread = RecordingThread(rec_queue)
-    matching_thread = MatchingThread(rec_queue, add_queue, aud, lock)
+    matching_thread = MatchingThread(rec_queue, add_queue, aud, aud_lock)
     recording_thread.start()
     matching_thread.start()
     
@@ -72,15 +94,15 @@ def main():
             j = new_input.find(">")
             if i != -1 and j != -1:
                 new_word = new_input[i+1:j]
-                with lock:
+                with aud_lock:
                     aud.add_recording(rec, label=new_word)
                     print(f"[{new_word} added]")
                     aud.save()
             else:
-                print(f"[{add_queue.qsize()} remain in queue]")
+                # reminder of how many "???" left to be assigned
+                print(f"[{add_queue.qsize()} remain in queue]") 
         else:
             print(f"[{add_queue.qsize()} remain in queue]")
-        
 
 if __name__ == "__main__":
     main()
