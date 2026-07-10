3376e545cd32e6bc2953a7591b221f9266848cb2
Hernan Le <hernanle2006@gmail.com>
2026-07-09 19:26:21 -0500

    switch threads around

--- eridian_translator.py
+++ eridian_translator.py
@@ -2,79 +2,84 @@
 
 # ----------------------------------------------------------------------
 
-from threading import Thread
+from threading import Thread, Lock
 from queue import Queue # used for passing data between threads
 
 from realtime_audio import record_word
 from audio_dictionary import AudioDictionary
 
-
 # ----------------------------------------------------------------------
 
-class AudioMatchingThread(Thread):
-    def __init__(self, aud:AudioDictionary, rec_queue:Queue, add_queue:Queue):
+class RecordingThread(Thread):
+    def __init__(self, rec_queue:Queue):
         Thread.__init__(self, daemon=True)
-        self._aud = aud
+        self._rec_queue = rec_queue
+    
+    def run(self):
+        while True:
+            recording = record_word()
+            self._rec_queue.put(recording)
+        
+class MatchingThread(Thread):
+    def __init__(self, rec_queue:Queue, add_queue:Queue, aud:AudioDictionary, lock:Lock):
+        Thread.__init__(self, daemon=True)
         self._rec_queue = rec_queue
         self._add_queue = add_queue
-    
-    def run(self):
-        while True:
-            # Dequeue data. Makes thread sleep if no data
-            rec = self._rec_queue.get()
-                
-            res = self._aud.match_recording(rec)
-            
-            if res["value"] is not None:
-                word = res["value"]["label"]
-                print(word, end=" ", flush=True)
-            else:
-                print("???", end=" ", flush=True)
-                self._add_queue.put(rec) # send to other thread if no match found
-                    
-                    
-
-class AudioAddingThread(Thread):
-    def __init__(self, aud:AudioDictionary, add_queue):
-        Thread.__init__(self, daemon=True)
         self._aud = aud
-        self._add_queue = add_queue
+        self._lock = lock
     
     def run(self):
         while True:
             # Dequeue data. Makes thread sleep if no data
-            rec = self._add_queue.get() 
+            rec = self._rec_queue .get()
             
-            # User input in the format of "<word>". 
-            # If no angle brackets, input for respective word is skipped.
-            new_input = input()
+            with self._lock:
+                res = self._aud.match_recording(rec)
+                
+                if res["value"] is not None:
+                    word = res["value"]["label"]
+                    print(word, end=" ", flush=True)
+                else:
+                    print("???", end=" ", flush=True)
+                    # send to main thread to get input if no match found
+                    self._add_queue.put(rec) 
             
-            if len(new_input) > 0:
-                i = new_input.find("<")
-                j = new_input.find(">")
-                if i != -1 and j != -1:
-                    new_word = new_input[i+1:j]
-                    self._aud.add_recording(rec, label=new_word)
-                    self._aud.save()
+
 
 # ----------------------------------------------------------------------
 
+# stdin/stdout on main thread
 def main():
-    aud = AudioDictionary(data_name="words", reset=False)
+    aud = AudioDictionary(data_name="words2", reset=False)
     rec_queue = Queue()
     add_queue = Queue()
+    lock = Lock()
     
-    match_thread = AudioMatchingThread(aud, rec_queue, add_queue)
-    add_thread = AudioAddingThread(aud,add_queue)
-    match_thread.start()
-    add_thread.start()
+    recording_thread = RecordingThread(rec_queue)
+    matching_thread = MatchingThread(rec_queue, add_queue, aud, lock)
+    recording_thread.start()
+    matching_thread.start()
     
     while True:
-        recording = record_word() # blocking until audio return
-        rec_queue.put(recording)
+        # Dequeue data. Makes thread sleep if no data
+        rec = add_queue.get()
         
-        
-        
+        # User input in the format of "<word>". 
+        # If no angle brackets, input for corresponding "???" is skipped.
+        new_input = input()
+        if len(new_input) > 0:
+            i = new_input.find("<")
+            j = new_input.find(">")
+            if i != -1 and j != -1:
+                new_word = new_input[i+1:j]
+                with lock:
+                    aud.add_recording(rec, label=new_word)
+                    print(f"[{new_word} added]")
+                    aud.save()
+            else:
+                print(f"[{add_queue.qsize()} remain in queue]")
+        else:
+            print(f"[{add_queue.qsize()} remain in queue]")
         
 
 if __name__ == "__main__":
